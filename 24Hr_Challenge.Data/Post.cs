7b63102e29e08ce100b0fd242790c64de93c09bf
sedutiel <samantha.dutiel@gmail.com>
2020-11-12 15:15:30 -0500

    created post

--- 24Hr_Challenge.Data/Post.cs
+++ 24Hr_Challenge.Data/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,15 @@ using System.Threading.Tasks;
 namespace _24Hr_Challenge.Data
 {
     public class Post
-    {
+    {   
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public Guid Author { get; set; }
+
+        
     }
 }
